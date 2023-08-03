ce135dc35e912d7f8e607f9b4829cbf648872d0e
rammex <romain.kaspar@gmail.com>
2023-08-03 16:53:20 +0200

    version 1.0

---
+++ src/main/java/org/rammex/minion_farmer/Minion_farmer.java
@@ -0,0 +1,21 @@
+package org.rammex.minion_farmer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Minion_farmer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("[Minion_farmer] Crée par .rammex");
+        getLogger().info("[Minion_farmer] Version 1.0");
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("[Minion_farmer] Crée par .rammex");
+        getLogger().info("[Minion_farmer] Version 1.0");
+
+    }
+}
